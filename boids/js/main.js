3f2457aead894b31994bcc4885d47c315eef5828
dugi <dugi@channel.io>
2022-04-23 21:58:02 +0900

    feat(boids): implement and show visible range around target particle

--- boids/js/main.js
+++ boids/js/main.js
@@ -15,16 +15,16 @@ async function main() {
   function createScene(){
     const scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color4(.1, .1, .1, 1);
-    const axesViewer = new BABYLON.AxesViewer(scene);
+    // const axesViewer = new BABYLON.AxesViewer(scene);
 
-    const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 20, BABYLON.Vector3.Zero(), scene);
+    const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 40, BABYLON.Vector3.Zero(), scene);
 
     const light = new BABYLON.HemisphericLight("Light", new BABYLON.Vector3(0, 1, 0), scene);
 
     const triangle = BABYLON.MeshBuilder.CreateDisc('Triangle', { tessellation: 3 }, scene);
   
     const SPS = new BABYLON.SolidParticleSystem('BoidSPS', scene);
-    SPS.addShape(triangle, 64);
+    SPS.addShape(triangle, 200);
 
     triangle.dispose();
 
@@ -33,18 +33,40 @@ async function main() {
     const colors = {
       light: new BABYLON.Color3(.43, .78, 1),
       dark: new BABYLON.Color3(0, .41, .75),
+      target: BABYLON.Color3.Red(),
     };
     const speed = 5.0;
-    const worldBoundary = {
-      x: { max: 15, min: -15 },
-      y: { max: 10, min: -10 },
+    const visibleRadius = 4.0;
+    const viewingAngle = 2/3 * Math.PI;
+
+    /**
+     * @param {BABYLON.SolidParticle} me 
+     * @param {BABYLON.SolidParticle} other 
+     * @returns {boolean}
+     */
+    function isVisibleParticle(me, other) {
+      const diff = other.position.subtract(me.position);
+      return (
+        diff.length() < visibleRadius &&
+        BABYLON.Vector3.Dot(
+          diff.clone().normalize(),
+          me.props.velocity.clone().normalize(),
+        ) >= Math.cos(viewingAngle)
+      );
     }
+
+    const worldBoundary = {
+      x: { max: 20, min: -20 },
+      y: { max: 15, min: -15 },
+    };
+
+    const targetParticleIndex = 0;
 
     /**
      * @param {BABYLON.Color3} one 
      * @param {BABYLON.Color3} other 
      * @param {number} t 
-     * @returns 
+     * @returns {BABYLON.Color3}
      */
     function mixColor(one, other, t) {
       const lerp = (a, b, t) => a * (1 - t) + b * t;
@@ -53,6 +75,35 @@ async function main() {
         lerp(one.g, other.g, t),
         lerp(one.b, other.b, t),
       );
+    }
+
+    /**
+     * @param {number} angle 
+     * @returns {BABYLON.Mesh}
+     */
+    function CreateArcMesh(angle) {
+      const nPartitions = Math.max(1, Math.ceil(angle / (.05 * Math.PI)));
+      const zeros = [];
+      const arc = [];
+      for (let i = -nPartitions; i <= nPartitions; i++) {
+        const a = (i / nPartitions) * angle;
+        arc.push(new BABYLON.Vector3(Math.cos(a), Math.sin(a), 0));
+        zeros.push(BABYLON.Vector3.Zero());
+      }
+      const options = {
+        pathArray: [
+          zeros,
+          arc,
+        ],
+      };
+      
+      const mesh = BABYLON.MeshBuilder.CreateRibbon('Arc', options, scene);
+
+      mesh.material = new BABYLON.StandardMaterial('ArcMesh', scene);
+      mesh.material.diffuseColor = new BABYLON.Color3(1, 1, 1);
+      mesh.material.alpha = .1;
+
+      return mesh;
     }
 
     /**
@@ -81,27 +132,62 @@ async function main() {
 
         particle.rotation.z = angle;
 
-        particle.props = { velocity };
+        particle.props = {
+          velocity,
+          acceleration: BABYLON.Vector3.Zero(),
+        };
 
         particle.scale = new BABYLON.Vector3(1, .5, 1);
 
-        particle.color = mixColor(colors.light, colors.dark, BABYLON.Scalar.RandomRange(0, 1));
+        particle.color = (
+          i === targetParticleIndex
+            ? colors.target
+            : mixColor(colors.light, colors.dark, BABYLON.Scalar.RandomRange(0, 1))
+        );
+
+        particle.props.color = particle.color;
       }
     }
 
     SPS.updateParticle = particle => {
-      const dt = engine.getDeltaTime();
+      const dt = engine.getDeltaTime() * .001;
 
-      particle.position.addInPlace(particle.props.velocity.scale(dt * .001));
+      particle.props.velocity.addInPlace(particle.props.acceleration.scale(dt));
+
+      particle.position.addInPlace(particle.props.velocity.scale(dt));
       sanitizePosition(particle.position, worldBoundary);
+
+      particle.rotation.z = Math.atan2(particle.props.velocity.y, particle.props.velocity.x);
     }
 
     SPS.initParticles();
     SPS.setParticles();
 
+    const viewingAreaMesh = CreateArcMesh(viewingAngle);
+
     scene.onBeforeRenderObservable.add(() => {
+      function markVisibleParticles() {
+        for (let i = 0; i < SPS.nbParticles; i++) {
+          if (i === targetParticleIndex) { continue; }
+          if (isVisibleParticle(SPS.particles[targetParticleIndex], SPS.particles[i])) {
+            SPS.particles[i].color = BABYLON.Color3.Yellow();
+          } else {
+            SPS.particles[i].color = SPS.particles[i].props.color;
+          }
+        }
+      }
+
+      function updateViewingAreaMesh() {
+        viewingAreaMesh.position.copyFrom(SPS.particles[targetParticleIndex].position);
+        viewingAreaMesh.rotation.copyFrom(SPS.particles[targetParticleIndex].rotation);
+        viewingAreaMesh.scaling.copyFromFloats(visibleRadius, visibleRadius, visibleRadius);
+      }
+
+      markVisibleParticles();
+      updateViewingAreaMesh();
+
       SPS.setParticles();
-    })
+    });
 
     return scene;
   }
