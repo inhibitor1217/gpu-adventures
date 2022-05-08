285f0fb950ca97c1db8926ac80e690372a8e76ed
dugi <dugi@channel.io>
2022-05-08 21:58:12 +0900

    refactor(boids-3d): separate some fns

--- boids-3d/js/main.js
+++ boids-3d/js/main.js
@@ -23,20 +23,35 @@ function main() {
   
   const engine = new Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
 
+  /**
+   * @param {BABYLON.Scene} scene
+   * @returns {BABYLON.Mesh}
+   */
+  function World(scene) {
+    const world = MeshBuilder.CreateBox('world', { size: $ENV.world.size }, scene);
+    world.position = $ENV.world.center;
+    world.material = new StandardMaterial('world-mat', scene);
+    world.material.wireframe = true;
+    return world;
+  }
+
+  /**
+   * @param {BABYLON.Scene} scene
+   * @param {BABYLON.Vector3} direction
+   * @returns {BABYLON.Light} 
+   */
+  function AmbientLight(scene, direction) {
+    return new HemisphericLight('ambient-light', direction, scene);
+  }
+
   function createScene(){
     const scene = new Scene(engine);
     const camera = new ArcRotateCamera('camera', .25 * Math.PI, .25 * Math.PI, 40, Vector3.Zero(), scene);
     camera.attachControl(canvas, false);
 
-    const world = MeshBuilder.CreateBox('world', { size: $ENV.world.size }, scene);
-    world.position = $ENV.world.center;
-    world.material = new StandardMaterial('world-mat', scene);
-    world.material.wireframe = true;
-  
-    const light = new HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-    const sphere = MeshBuilder.CreateSphere('sphere1', {segments: 16, diameter: 2, sideOrientation: Mesh.FRONTSIDE}, scene);
-    sphere.position.y = 1;
-    const ground = MeshBuilder.CreateGround("ground1", { width: 6, height: 6, subdivisions: 2, updatable: false }, scene);
+    const world = World(scene);  
+    const light = AmbientLight(scene, Vector3.Up());
+
     return scene;
   }
 
