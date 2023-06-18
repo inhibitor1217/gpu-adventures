daf40c5a6f8bd932a1affbac315ec0f61271428c
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-06-18 21:35:29 +0900

    write wgsl shaders with bjs

--- star/src/main.ts
+++ star/src/main.ts
@@ -1,27 +1,83 @@
 import {
+  ArcRotateCamera,
   Engine,
-  FreeCamera,
-  HemisphericLight,
   MeshBuilder,
   Scene,
+  ShaderLanguage,
+  ShaderMaterial,
+  ShaderStore,
   Vector3,
   WebGPUEngine,
 } from 'babylonjs'
 
 import './style.css'
 
-function createScene(engine: Engine) {
+async function prepareShaders(): Promise<void> {
+  ShaderStore.ShadersStoreWGSL['customVertexShader'] = `
+  #include<sceneUboDeclaration>
+  #include<meshUboDeclaration>
+
+  attribute position: vec3<f32>;
+  attribute normal: vec3<f32>;
+
+  varying vNormal: vec3<f32>;
+
+  @vertex
+  fn main(input: VertexInputs) -> FragmentInputs {
+    vertexOutputs.position = scene.viewProjection * mesh.world * vec4<f32>(vertexInputs.position, 1.0);
+    vertexOutputs.vNormal = normalize((mesh.world * vec4<f32>(vertexInputs.normal, 0.0)).xyz);
+  }
+  `
+
+  ShaderStore.ShadersStoreWGSL['customFragmentShader'] = `
+  varying vNormal: vec3<f32>;
+
+  @fragment
+  fn main(input: FragmentInputs) -> FragmentOutputs {
+    var diffuse: f32 = clamp(dot(fragmentInputs.vNormal, vec3<f32>(0.0, 1.0, 0.0)), 0.0, 1.0);
+    var lightColor: vec3<f32> = vec3<f32>(1.0, 1.0, 1.0);
+    fragmentOutputs.color = vec4<f32>(diffuse * lightColor, 1.0);
+  }
+  `
+}
+
+async function prepare(canvas: HTMLCanvasElement): Promise<WebGPUEngine> {
+  const engine = new WebGPUEngine(canvas)
+
+  await engine.initAsync()
+  await prepareShaders()
+
+  window.addEventListener('resize', () => {
+    engine.resize()
+  })
+
+  return engine
+}
+
+async function createScene(engine: Engine): Promise<Scene> {
   const scene = new Scene(engine)
 
-  const camera = new FreeCamera("pov", new Vector3(0, 5, -10), scene)
+  const camera = new ArcRotateCamera('camera', 0.25 * Math.PI, 0.25 * Math.PI, 5, Vector3.Zero(), scene)
   camera.setTarget(Vector3.Zero())
   camera.attachControl(engine.getRenderingCanvas(), true)
 
-  const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene)
-  light.intensity = 0.7
+  const shaderMaterial = new ShaderMaterial(
+    'custom',
+    scene,
+    {
+      vertex: 'custom',
+      fragment: 'custom',
+    },
+    {
+      attributes: ['position', 'normal'],
+      uniformBuffers: ['Scene', 'Mesh'],
+      shaderLanguage: ShaderLanguage.WGSL,
+    },
+  )
 
-  const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene)
-  sphere.position.y = 0
+  const sphere = MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene)
+  sphere.material = shaderMaterial
+  sphere.position = Vector3.Zero()
 
   return scene
 }
@@ -29,17 +85,11 @@ function createScene(engine: Engine) {
 async function main() {
   const canvas = document.getElementById('root') as HTMLCanvasElement
 
-  const engine = new WebGPUEngine(canvas)
-  await engine.initAsync()
-  
-  const scene = createScene(engine)
+  const engine = await prepare(canvas)
+  const scene = await createScene(engine)
 
   engine.runRenderLoop(() => {
     scene.render()
-  })
-
-  window.addEventListener('resize', () => {
-    engine.resize()
   })
 }
 
